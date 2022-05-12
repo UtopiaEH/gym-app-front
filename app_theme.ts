0be2e976aa8bfe9d5ae79aa7658ef9f264e58508
UtopiaEH <utopiaeh01@gmail.com>
2022-05-12 20:06:55 +0300

    settings

---
+++ app_theme.ts
@@ -0,0 +1,20 @@
+export const app_theme = {
+    'type': 'light', // light / dark
+    // 'className': '', // optional
+    'theme': {
+        'colors': {},
+        'space': {},
+        'fontSizes': {},
+        'fonts': {},
+        'fontWeights': {},
+        'lineHeights': {},
+        'letterSpacings': {},
+        'sizes': {},
+        'borderWidths': {},
+        'borderStyles': {},
+        'radii': {},
+        'shadows': {},
+        'zIndices': {},
+        'transitions': {}
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { NextUIProvider, theme } from '@nextui-org/react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { app_theme } from '../app_theme'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+    document.getElementById('root') as HTMLElement
+)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <NextUIProvider>
+            <App />
+        </NextUIProvider>
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
